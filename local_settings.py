2e27e2b3a78ca962ded8c0e9ed2d3173be9ef4e4
master <2534891955@qq.com>
2023-01-10 10:58:45 +0800

    init

---
+++ local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "gdut-trading",  # noqa
+        "USER": "root",
+        "PASSWORD": "2534891955!",
+        "HOST": "localhost",
+        "PORT": "3306",
+    },
+}
